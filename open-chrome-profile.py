84c603f63f8d17a95844c9984072415086da511b
subv3rsive <89690792+subv3rsive@users.noreply.github.com>
2021-12-01 12:52:49 -0500

    Update open-chrome-profile.py
    added profile counter for yung kayak

--- open-chrome-profile.py
+++ open-chrome-profile.py
@@ -2,11 +2,19 @@ import subprocess
 import os
 import re
 
+# USER OPTIONS
+#-----------------------------------------
 # Path to the Chrome executable.
 CHROME_EXE_PATH = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
 # Path to the profile directory.
 CHROME_FOLDER_PATH = r"%localappdata%\Google\Chrome\User Data"
+
+# Number of profiles to open
+NUM_PROFILES = 3
+
+# END USER OPTIONS
+#-----------------------------------------
 
 # Expand %localappdata% into a full system path.
 expanded = os.path.expandvars(CHROME_FOLDER_PATH)
@@ -27,6 +35,12 @@ for f in os.listdir(expanded):
 profiles = sorted(profiles, key=lambda x: int(x.split(" ")[1]))
 
 # Launch chrome with the profile.
+count = 1
 for p in profiles:
     arg = f"--profile-directory={p}"
     subprocess.check_call([CHROME_EXE_PATH, arg])
+
+    if count >= NUM_PROFILES:
+        break
+        
+    count += 1
